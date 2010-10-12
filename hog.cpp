d2d0aeb34630f4c2cba2d246d37050e4d7fac7b6
Ben M. Ward <ben.ward@gmail.com>
2010-10-11 22:05:26 -0600

    Fixed a glitch where feof() failed to return true on the end of file

--- hog.cpp
+++ hog.cpp
@@ -35,7 +35,7 @@ vector<string> CHog::get_Filenames() const
 		if(3 == fread(signature, sizeof(char), 3, fp)) {
 			if(signature[0] == 'D' && signature[1] == 'H' && signature[2]) {
 				while(!feof(fp)) {
-					fread(file_name, sizeof(char), 13, fp);
+					if(13 != fread(file_name, sizeof(char), 13, fp)) break; //Exit if feof() fails to return end of file
 					string name = file_name;
 					retval.push_back(name);
 					fread(&file_size, sizeof(int), 1, fp);
