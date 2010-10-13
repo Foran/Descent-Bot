e31dce3fcad89453f20d3820994fd0cda2bc9d42
Ben M. Ward <ben.ward@gmail.com>
2010-10-13 00:00:04 -0600

    Initial Import

---
+++ rdl.cpp
@@ -0,0 +1,56 @@
+#include "rdl.h"
+
+CRdl::CRdl()
+{
+}
+
+
+CRdl::CRdl(const string &filename)
+{
+}
+
+CRdl::CRdl(const CHog &hog, const string &filename)
+{
+}
+
+CRdl::CRdl(const string &hog, const string &filename)
+{
+}
+
+CRdl::CRdl(const CRdl &source)
+{
+}
+
+CRdl::~CRdl()
+{
+}
+
+CRdl &CRdl::operator=(const CRdl &source)
+{
+   return *this;
+}
+
+void CRdl::Reset()
+{
+}
+
+bool CRdl::Load(const string &filename)
+{
+   bool retval = false;
+   
+   return retval;
+}
+
+bool CRdl::Load(const CHog &hog, const string &filename)
+{
+   bool retval = false;
+   
+   return retval;
+}
+
+bool CRdl::Load(const string &hog, const string &filename)
+{
+   bool retval = false;
+   
+   return retval;
+}
---
+++ rdl.h
@@ -0,0 +1,42 @@
+#ifndef __RDL_H__
+#define __RDL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "hog.h"
+
+class CRdl {
+ public:
+   CRdl();
+   CRdl(const string &filename);
+   CRdl(const CHog &hog, const string &filename);
+   CRdl(const string &hog, const string &filename);
+   CRdl(const CRdl &source);
+   ~CRdl();
+   
+   CRdl &operator=(const CRdl &source);
+   
+   void Reset();
+   bool Load(const string &filename);
+   bool Load(const CHog &hog, const string &filename);
+   bool Load(const string &hog, const string &filename);
+ protected:
+ private:
+   struct {
+      char signature[4];
+      long version;
+      long mineDataOffset;
+      long objectsOffset;
+      long fileSize;
+   } mHeader;
+};
+
+#endif
+
