f10668db3c9d41d4a4f02646ac51ff4390bdb0f9
Ben M. Ward <ben.ward@gmail.com>
2011-03-19 18:23:51 -0600

    Initial import of the connection object

---
+++ connection.h
@@ -0,0 +1,13 @@
+#ifndef __CONNECTION_H__
+#define __CONNECTION_H__
+
+class CConnection 
+{
+ public:
+ protected:
+ private:
+     CConnection(const CConnection &source);
+   CConnection &operator=(const CConnection &source);
+};
+
+#endif
