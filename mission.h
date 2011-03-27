e2420a074973e47e1a05a05466cffbb3d8e21282
Ben M. Ward <ben.ward@gmail.com>
2011-03-26 23:36:40 -0600

    Added initial class definitions for mission and missionmanager

--- mission.h
+++ mission.h
@@ -1,4 +1,11 @@
 #ifndef __MISSION_H__
 #define __MISSION_H__
 
+class CMission 
+{
+ public:
+ protected:
+ private:
+};
+
 #endif
--- missionmanager.h
+++ missionmanager.h
@@ -1,4 +1,11 @@
 #ifndef __MISSIONMANAGER_H__
 #define __MISSIONMANAGER_H__
 
+class CMissionManager
+{
+ public:
+ protected:
+ private:
+};
+
 #endif
