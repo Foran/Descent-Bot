d91902bdf831c3b9f09a2008c31726f6e13c5904
Ben M. Ward <ben.ward@gmail.com>
2011-04-09 15:11:09 -0600

    Initial signal handler

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include "main.h"
 
+/**************************
+ * This is temporary implementation
+ * This will go into a seperate class
+ *************************/
+#include <signal.h>
+
+bool dbot_signal_stillwantstoplay = true;
+
+void dbot_signal_handler(int s)
+{
+   dbot_signal_stillwantstoplay = false;
+}
+
 int main(int argc, char **argv)
 {
   int retval = 0;
+   CConnectionManager ConnectionManager;
    
-   global_Config.Load("config/Main.xml");
+   struct sigaction sigIntHandler;
+   sigIntHandler.sa_sigaction = NULL;
+   sigIntHandler.sa_handler = dbot_signal_handler;
+   sigemptyset(&sigIntHandler.sa_mask);
+   sigIntHandler.sa_flags = 0;
+   sigIntHandler.sa_restorer = NULL;
+   sigaction(SIGINT, &sigIntHandler, NULL);
    
-   vector<string> names = HogManager["chaos.hog"].get_Filenames();
-   for(vector<string>::iterator i = names.begin(); i != names.end(); i++) {
-      cout << *i << endl;
-   }
+//   global_Config.Load("config/Main.xml");
    
-   CRdl rdl1("chaos.hog", "chaos1.rdl");
-   CRdl rdl2("chaos.hog", "chaos2.rdl");
+//   vector<string> names = HogManager["chaos.hog"].get_Filenames();
+//   for(vector<string>::iterator i = names.begin(); i != names.end(); i++) {
+//      cout << *i << endl;
+//   }
+   
+//   CRdl rdl1("chaos.hog", "chaos1.rdl");
+//   CRdl rdl2("chaos.hog", "chaos2.rdl");
 
-   CConnectionManager::get_Instance().Pulse();
+   while(dbot_signal_stillwantstoplay) CConnectionManager::get_Instance().Pulse();
 //   CConnection connection;
 //   connection.find_Game();
    
