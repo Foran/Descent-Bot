1b2071a78a1f6b9eb2fb4b9ef0a09f6d3cdc1408
Ben M. Ward <ben.ward@gmail.com>
2011-06-25 10:55:33 -0600

    Added Legal Comment Headers and fixed line endings to be consistant

--- test.h
+++ test.h
@@ -1,3 +1,13 @@
+/****************************************************
+ * This work is licensed under the Creative
+ * Commons Attribution-NonCommercial-ShareAlike
+ * 3.0 Unported License. To view a copy of this
+ * license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * or send a letter to Creative Commons, 444
+ * Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ ***************************************************/
 #ifndef __TEST_H__
 #define __TEST_H__
 
