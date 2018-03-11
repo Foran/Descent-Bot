69e9ad5e34c59e6874770229632375157c9ce4fe
Ben M. Ward <ben.ward@gmail.com>
2018-03-10 21:19:05 -0800

    added new network test

---
+++ src/lib/network/test/packetUnitTests.cc
@@ -0,0 +1,52 @@
+/****************************************************
+ * Copyright 2018 Ben M. Ward
+ *
+ * This work is licensed under the Creative
+ * Commons Attribution-NonCommercial-ShareAlike
+ * 3.0 Unported License. To view a copy of this
+ * license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * or send a letter to Creative Commons, 444
+ * Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ ***************************************************/
+#include <string>
+
+#include "gtest/gtest.h"
+#include "src/lib/network/packets.h"
+
+using ::DESCENT_BOT::SRC::LIB::NETWORK::PACKET_Game_Info_Lite;
+using ::DESCENT_BOT::SRC::LIB::NETWORK::UDP_PacketType;
+using ::std::string;
+
+class PacketTest : public ::testing::Test {
+ protected:
+  PacketTest() {
+  }
+
+  virtual ~PacketTest() {
+  }
+
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+
+ private:
+};
+
+TEST_F(PacketTest, DefaultConstructor) {
+  unsigned char buffer[] = {0x05, 0x00, 0x00, 0x38, 0x00, 0x00, 0x00, 0x66,
+                            0x6f, 0x72, 0x61, 0x6e, 0x27, 0x73, 0x20, 0x67,
+                            0x61, 0x6d, 0x65, 0x00, 0x00, 0x00, 0x00, 0x54,
+                            0x6f, 0x74, 0x61, 0x6c, 0x20, 0x43, 0x68, 0x61,
+                            0x6f, 0x73, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x48, 0x41,
+                            0x4f, 0x53, 0x00, 0x00, 0x00, 0x00};
+
+  PACKET_Game_Info_Lite *packet =
+                         reinterpret_cast<PACKET_Game_Info_Lite *>(buffer);
+  EXPECT_EQ(UDP_PacketType::UPID_GAME_INFO_LITE, packet->Type);
+  EXPECT_EQ(string("foran's game"), string(packet->Game_Name));
+}
